02c07062033e687cb57c036512537ee21aaa4912
ohnj8800 <ohnj8800@gmail.com>
2025-11-28 17:42:17 +0800

    描述更新內容

--- hashed_password.py
+++ hashed_password.py
@@ -2,7 +2,6 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-for i in range(3, 21):  # 03 到 20
-    password = f"test{i:02d}"   # ← 重點：補零到兩位數
-    hashed = pwd_context.hash(password)
-    print(f"{password} -> {hashed}")
+password = "Student"   # ← 重點：補零到兩位數
+hashed = pwd_context.hash(password)
+print(f"{password} -> {hashed}")
